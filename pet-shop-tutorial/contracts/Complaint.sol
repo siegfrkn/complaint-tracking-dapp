0dabf368d7a9a66c8b0dcf2f4dd80943e5629a86
siegfrkn <katrina.siegfried@gmail.com>
2023-04-22 14:50:36 -0600

    initial addition for complaint contract

---
+++ pet-shop-tutorial/contracts/Complaint.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
+
+contract Complaint {
+    // Store candidate
+    // Read candidate
+    string public complaint;
+    // Constructor
+    constructor () public {
+        complaint = "Complaint 1";
+    }
+}
