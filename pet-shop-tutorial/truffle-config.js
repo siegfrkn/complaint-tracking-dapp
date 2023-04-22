753a1251639adeb6016ec1f29e52bafc059280a9
siegfrkn <katrina.siegfried@gmail.com>
2023-04-22 11:47:10 -0600

    complete tutorial successfully, browser still launching twice

--- pet-shop-tutorial/truffle-config.js
+++ pet-shop-tutorial/truffle-config.js
@@ -3,12 +3,9 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
+      host: "172.19.64.1",
+      port: 7545,
       network_id: "*" // Match any network id
     },
-    develop: {
-      port: 8545
-    }
-  }
+  },
 };
