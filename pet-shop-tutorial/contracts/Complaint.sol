aedb014927d71a0a2c403f34b9e35590fd42fe76
siegfrkn <katrina.siegfried@gmail.com>
2023-04-22 15:36:06 -0600

    update contract functionality

--- pet-shop-tutorial/contracts/Complaint.sol
+++ pet-shop-tutorial/contracts/Complaint.sol
@@ -2,11 +2,30 @@
 pragma solidity >=0.5.0;
 
 contract Complaint {
-    // Store candidate
-    // Read candidate
-    string public complaint;
+    // Model a complaint
+    struct Entry {
+        uint id;
+        string name;
+        // uint capa;
+        // string entryType; // TODO: Change to enum
+        // string reporter;
+        // string site;
+        // string description;
+        // string impact; // TODO Change to enum
+    }
+    // Store entry
+    // Fetch entry
+    mapping(uint => Entry) public entries;
+    // Store entry count
+    uint public entriesCount;
     // Constructor
     constructor () public {
-        complaint = "Complaint 1";
+        addEntry("Complaint 1");
+        addEntry("Complaint 2");
+    }
+
+    function addEntry (string memory _name) public {
+        entriesCount++;
+        entries[entriesCount] = Entry(entriesCount, _name);
     }
 }
--- pet-shop-tutorial/truffle-config.js
+++ pet-shop-tutorial/truffle-config.js
@@ -3,10 +3,10 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      // host: "172.19.64.1",
-      port: 8545,
-      // port: 7545,
+      // host: "127.0.0.1",
+      host: "172.19.64.1",
+      // port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
     },
   },
