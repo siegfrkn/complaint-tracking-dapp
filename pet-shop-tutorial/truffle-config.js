6217ae993cb1b1f2fa05877774c71f9007f9145a
Katrina <katrina.siegfried@gmail.com>
2023-04-26 20:59:45 -0600

    Remove commented out code

--- pet-shop-tutorial/truffle-config.js
+++ pet-shop-tutorial/truffle-config.js
@@ -3,9 +3,7 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      // host: "127.0.0.1",
-      host: "172.19.64.1",
-      // port: 8545,
+      host: "172.19.64.1", // IP of Ganache host
       port: 7545,
       network_id: "*" // Match any network id
     },
